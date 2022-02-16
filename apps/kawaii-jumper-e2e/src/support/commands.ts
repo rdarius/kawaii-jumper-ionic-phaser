f563adc289cf02e6780c18d7c42263d7d2c81ab8
Darius Rapalis <info@rdarius.lt>
2022-02-16 22:11:47 +0200

    removed password from console.log

--- apps/kawaii-jumper-e2e/src/support/commands.ts
+++ apps/kawaii-jumper-e2e/src/support/commands.ts
@@ -18,7 +18,8 @@ declare namespace Cypress {
 //
 // -- This is a parent command --
 Cypress.Commands.add('login', (email, password) => {
-    console.log('Custom command example: Login', email, password);
+    password.split('');
+    console.log('Custom command example: Login', email);
 });
 //
 // -- This is a child command --
